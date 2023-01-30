8c20dcb016158e8b7662ede9402ddd4a1a4f975f
leenock <leenock176@gmail.com>
2023-01-30 10:41:36 +0800

    today update

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <sections className="skills sections" id="skills">
+        <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My Technical Level</span>
+      </sections>
+  )
+}
+
+export default Skills
