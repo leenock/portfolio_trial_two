f4b6e6815effa022eec48af496dc7a3cb2ea05b6
leenock <leenock176@gmail.com>
2023-02-01 11:33:54 +0800

    setup gh-pages

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import "./skills.css";
+import Frontend from './Frontend';
+import Backend from './Backend';
 
 const Skills = () => {
   return (
     <sections className="skills sections" id="skills">
         <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">My Technical Level</span>
+        <div className="skills__container container grid">
+            <Frontend />
+            <Backend />
+        </div>
       </sections>
   )
 }
